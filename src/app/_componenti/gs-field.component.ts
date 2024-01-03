a9d1c5e1ef190d3db0ad39cb23d69130e2bb9880
Rino <rinosan76@gmail.com>
2024-01-03 20:05:34 +0100

    Eliminato ngSwitch da gs-field

--- src/app/_componenti/gs-field.component.ts
+++ src/app/_componenti/gs-field.component.ts
@@ -1,6 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TTypeFiled } from './gs-form-type';
 import { GsInputComponent } from './gs-input.component';
 import { GsCheckboxComponent } from './gs-checkbox.component';
@@ -10,6 +19,7 @@ import { GsSelectComponent } from './gs-select.component';
 @Component({
   selector: 'gs-field',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -18,29 +28,66 @@ import { GsSelectComponent } from './gs-select.component';
     GsRadioComponent,
     GsSelectComponent,
   ],
-  template: `
-    @switch (field.type) { @case('select'){
-    <gs-select [field]="field"></gs-select>
-    } @case ('checkbox') {
-    <gs-checkbox [field]="field"></gs-checkbox>
-    } @case ('radio') {
-    <gs-radio [field]="field"></gs-radio>
-    } @default {
-    <gs-input [field]="field"></gs-input>
-    } }
-  `,
+  template: ` <ng-container #contCampo> </ng-container> `,
   styles: ``,
 })
-export class GsFieldComponent implements OnInit {
+export class GsFieldComponent implements OnInit, AfterViewInit {
   //-------------------------------------------------------------------
   //PUBLIC
   @Input() field!: TTypeFiled;
+  @ViewChild('contCampo', { read: ViewContainerRef })
+  contCampo!: ViewContainerRef;
 
   //-------------------------------------------------------------------
   //PRIVATE
+  private componentiSupportati = [
+    {
+      name: 'text',
+      component: GsInputComponent,
+    },
+    {
+      name: 'number',
+      component: GsInputComponent,
+    },
+    {
+      name: 'select',
+      component: GsSelectComponent,
+    },
+    {
+      name: 'radio',
+      component: GsRadioComponent,
+    },
+    {
+      name: 'date',
+      component: GsInputComponent,
+    },
+    {
+      name: 'checkbox',
+      component: GsCheckboxComponent,
+    },
+  ];
 
+  //-------------------------------------------------------------------
+  constructor(private cd: ChangeDetectorRef) {}
   //-------------------------------------------------------------------
   ngOnInit(): void {
     // console.log('FIELD', this.field);
+  }
+  //-------------------------------------------------------------------
+  ngAfterViewInit(): void {
+    this.registraCampi();
+  }
+  //-------------------------------------------------------------------
+  private registraCampi() {
+    this.contCampo.clear();
+    const tmp = this.selezionaTipo(this.field.type);
+    const componente = this.contCampo.createComponent(tmp);
+    componente.setInput('field', this.field);
+    this.cd.detectChanges();
+  }
+  //-------------------------------------------------------------------
+  selezionaTipo(type: string): any {
+    const rit = this.componentiSupportati.find((c) => c.name === type);
+    return rit?.component || GsInputComponent;
   }
 }
